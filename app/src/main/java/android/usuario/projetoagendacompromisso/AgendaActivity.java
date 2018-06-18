b169b93954d865bf7d4db9ec3d3ddbe3817f92f2
João Carlos <joaoc9074@gmail.com>
2018-06-18 16:03:48 -0300

    Adicionado os comandos do btCadastro e btLista

--- app/src/main/java/android/usuario/projetoagendacompromisso/AgendaActivity.java
+++ app/src/main/java/android/usuario/projetoagendacompromisso/AgendaActivity.java
@@ -1,13 +1,38 @@
 package android.usuario.projetoagendacompromisso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AgendaActivity extends AppCompatActivity {
+
+    Button btCadastro, btLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+
+        btCadastro = (Button) findViewById(R.id.btCadastro);
+        btCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(AgendaActivity.this, CadActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btLista = (Button) findViewById(R.id.btLista);
+        btLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(AgendaActivity.this, ListaActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
